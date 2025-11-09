af31a80f96e2224870277cfecfb4031ba6d30e81
Grancho <guilhermegranchopro@gmail.com>
2025-11-09 19:14:01 +0000

    Adjust margin on About page to zero for improved layout consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function About() {
       <Column
         style={{
           width: "100%",
-          marginTop: "16px",
+          marginTop: "0px",
           marginBottom: "32px",
         }}
         horizontal="center"
